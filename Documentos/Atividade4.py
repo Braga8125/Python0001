bf2c48e30694ac301413f21d424f4e9c6c6008d9
Braga8125 <andreiadeya955@gmail.com>
2023-08-09 16:44:40 -0400

    Atividade 4

---
+++ Documentos/Atividade4.py
@@ -0,0 +1,30 @@
+"""
+1.Crie uma função em Python para retornar a área de um retângulo, considere a seguinte fórmula:A=b.h
+"""
+# def calcular_area(a1,a2):
+#     return(a1*a2)
+
+# print(calcular_area(7,6))
+"""
+2.Crie uma função em Python para mostrar a área de um círculo, considere a seguinte fórmula:A=3,14*r*r
+"""
+# def circulo_area (r):
+#     return(3.14*r*r)
+# print(circulo_area(7))
+"""
+3.	Crie uma função em Python para mostrar o produto da multiplicação entre n valores.
+"""
+
+def produto_multi(*args):
+    return args*args
+print(produto_multi(5,2))
+
+"""
+4.	Crie uma função em Python para mostrar apenas as chaves de um dicionário.
+"""
+
+"""
+5.	Crie uma função em Python para mostrar apenas os valores de um dicionário.
+"""
+
+
