baec65aa0babb48d5483342e4230272e49f230be
Braga8125 <andreiadeya955@gmail.com>
2023-08-09 16:03:50 -0400

    Atividade 4 e extra do python

--- Aula08/exemplo4.py
+++ Aula08/exemplo4.py
@@ -1,8 +1,19 @@
 #Soma de n valores
+from ast import arg
+
+
 def soma_coxinha(*args):
     return sum(args)
 def media_valores(*args):
     return sum(args)/len(args)
+def valores (*args):
+    return type(args)
+def valores_teste (**kwargs):#key words arguments
+    return type(kwargs)
+
+
+
+
 if __name__ == '__main__':
     
     print(soma_coxinha(6,10)) 
@@ -12,3 +23,5 @@ if __name__ == '__main__':
     print(media_valores(10,8,6))
     print(media_valores(10,10,10,10,10))
     print(media_valores(10,2,2))
+    print(valores(5,6,9,8))
+    print(valores_teste(idade=3,preco=6,qtd=9))
